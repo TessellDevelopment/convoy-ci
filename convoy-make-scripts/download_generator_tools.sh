f99921abd09181877a454aa63b3c99e40058de98
ayushtessell <ayushman.mishra@tessell.com>
2026-04-17 03:24:42 +0530

    Revert "add ci-test-repo-a"
    This reverts commit cba3c40be89b4981d57ae57c86b75168086e7868.

---
+++ convoy-make-scripts/download_generator_tools.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+VERSION=${1}
+
+NEXUS_PASSWORD=${NEXUS_PASSWORD:-${GITHUB_TOKEN}}
+NEXUS_USERNAME=${NEXUS_USERNAME:-${GITHUB_USER}}
+
+OPENAPI_GENERATOR_CLI_URL=https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.8.0/openapi-generator-cli-7.8.0.jar
+NEXUS_URL=${NEXUS_PROTOCOL:-https}://${NEXUS_USERNAME}:${NEXUS_PASSWORD}@${NEXUS_SERVER_ENDPOINT:-nexus.tessell.cloud}/repository/${NEXUS_PULL_REPOS_M2:-tessell-m2-development}
+
+CONVOY_API_SPEC_TRANSPILER_URL=${NEXUS_URL}/convoy/code-generator/convoy-api-spec-transpiler/${VERSION}/convoy-api-spec-transpiler-${VERSION}.jar
+CONVOY_OPENAPI_CODE_GENERATOR_URL=${NEXUS_URL}/convoy/code-generator/convoy-openapi-code-generator/${VERSION}/convoy-openapi-code-generator-${VERSION}.jar
+
+function download_jars() {
+    curl ${CONVOY_API_SPEC_TRANSPILER_URL} -O
+    curl ${CONVOY_OPENAPI_CODE_GENERATOR_URL} -O 
+    curl ${OPENAPI_GENERATOR_CLI_URL} -O
+}
+
+mkdir generator_tools && cd ./generator_tools 
+download_jars
